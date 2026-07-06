5bf96e1556c951aa4789c77faf3f54f6be030a4d
francesco-mauro <francesco.mauro180@gmail.com>
2026-07-06 17:37:26 +0200

    Add comment to clarify store provider purpose in main.tsx

--- myLoginTest/src/main.tsx
+++ myLoginTest/src/main.tsx
@@ -1,6 +1,7 @@
 // src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+// rende lo store disponibile a tutti i componenti
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './store/store';
